671f40ae534c34ee5f1f53d59dfbf79d360e4499
guasc0 <gual.scolari@hotmail.com>
2016-11-15 13:40:46 +0100

    sdfsdf

--- app/src/main/java/com/example/gualbertoscolari/grupp3/MenuActivity.java
+++ app/src/main/java/com/example/gualbertoscolari/grupp3/MenuActivity.java
@@ -1,7 +1,9 @@
 package com.example.gualbertoscolari.grupp3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,6 +11,7 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        int x;
+
     }
+
 }
