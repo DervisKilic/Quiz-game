e9de77c67ad6a7f9d7987e0e22f8f7966d4296d3
Dervis <dervis85@gmail.com>
2016-11-16 13:18:12 +0100

    fixes

--- app/src/main/java/com/example/gualbertoscolari/grupp3/CreateQuestionActivity.java
+++ app/src/main/java/com/example/gualbertoscolari/grupp3/CreateQuestionActivity.java
@@ -3,32 +3,15 @@ package com.example.gualbertoscolari.grupp3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-
-import java.util.ArrayList;
-
-import static android.R.attr.category;
 
 
 public class CreateQuestionActivity extends AppCompatActivity {
-    private ArrayList<String> category = new ArrayList<>();
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_question);
-
-        category.add("Choose your category");
-        category.add("Sport");
-        category.add("Nature");
-        category.add("Culture");
-        category.add("All");
-        Spinner dropdown = (Spinner) findViewById(R.id.category_spinner);
-        ArrayAdapter<String> chosenCategory = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, category);
-        dropdown.setAdapter(chosenCategory);
 
     }
 
