49a63886dfb51905cc432b951ca0113c8ab4cb7f
guasc0 <gual.scolari@hotmail.com>
2016-11-16 20:46:21 +0100

    Added abstract methods to CreateAndDelete

--- app/src/main/java/com/example/gualbertoscolari/grupp3/CreateAndDelete.java
+++ app/src/main/java/com/example/gualbertoscolari/grupp3/CreateAndDelete.java
@@ -1,4 +1,38 @@
 package com.example.gualbertoscolari.grupp3;
 
 public class CreateAndDelete {
+
+    public void createProfile(){
+        //Creates a profile and uses DBHelper to add it
+        //to the Data Base
+        //Takes a specific category as argument
+    }
+
+    public void createCategory(){
+        //Creates a category and uses DBHelper to add it
+        //to the Data Base
+        //Takes a specific category as argument
+    }
+
+    public void createQuestion(){
+        //Creates a question and uses DBHelper to add it
+        //to the Data Base
+        //Takes a specific category as argument
+    }
+
+    public void deleteProfile(){
+        //Takes a specific profile as an argument and
+        //deletes with DBHelper
+
+    }
+
+    public void deleteCategory(){
+        //Takes a specific category as an argument and
+        //deletes with DBHelper
+    }
+
+    public void deleteQuestion(){
+        //Takes a specific question as an argument and
+        //deletes with DBHelper
+    }
 }
--- app/src/main/java/com/example/gualbertoscolari/grupp3/CreateCategoryActivity.java
+++ app/src/main/java/com/example/gualbertoscolari/grupp3/CreateCategoryActivity.java
@@ -3,14 +3,19 @@ package com.example.gualbertoscolari.grupp3;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 
+
+
 public class CreateCategoryActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_category);
+
     }
 
     public void goToGameSettings(View v){
