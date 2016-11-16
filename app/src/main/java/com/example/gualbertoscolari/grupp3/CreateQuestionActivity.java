476f4f375e4d16e29264e95d07ddcdccafd505a5
guasc0 <gual.scolari@hotmail.com>
2016-11-16 10:17:26 +0100

    Added spinner to Create question

--- app/src/main/java/com/example/gualbertoscolari/grupp3/CreateQuestionActivity.java
+++ app/src/main/java/com/example/gualbertoscolari/grupp3/CreateQuestionActivity.java
@@ -1,13 +1,34 @@
 package com.example.gualbertoscolari.grupp3;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+import static android.R.attr.category;
+
 
 public class CreateQuestionActivity extends AppCompatActivity {
+    private ArrayList<String> category = new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_question);
+
+        category.add("Sport");
+        category.add("Nature");
+        category.add("Culture");
+        category.add("All");
+        Spinner dropdown = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> chosenCategory = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, category);
+        dropdown.setAdapter(chosenCategory);
+
     }
+
 }
