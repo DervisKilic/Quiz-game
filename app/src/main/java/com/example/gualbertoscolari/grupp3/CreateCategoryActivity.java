e353a52923988e734f41dbe3c084a96b97e23906
Dervis <dervis85@gmail.com>
2016-11-17 11:20:54 +0100

    Merge branch 'master' of /Users/Dervis/AndroidStudioProjects/Grupp3 with conflicts.

--- app/src/main/java/com/example/gualbertoscolari/grupp3/CreateCategoryActivity.java
+++ app/src/main/java/com/example/gualbertoscolari/grupp3/CreateCategoryActivity.java
@@ -1,13 +1,38 @@
 package com.example.gualbertoscolari.grupp3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class CreateCategoryActivity extends AppCompatActivity {
+
+    private String category = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_category);
+
+    }
+
+    public void goToGameSettings(View v){
+        Intent gameSettings = new Intent(this, GameSettingsActivity.class);
+        startActivity(gameSettings);
+    }
+
+    public void addCategory(View v){
+        // Calls the addCategory method in the CreateAndDelete class
+        // and uses DBHelper to add the category
+        randomWord = (EditText) findViewById(R.id.current_word);
+    }
+
+    public String getCategory() {
+        return category;
     }
 }
--- app/src/main/java/com/example/gualbertoscolari/grupp3/CreateQuestionActivity.java
+++ app/src/main/java/com/example/gualbertoscolari/grupp3/CreateQuestionActivity.java
@@ -3,9 +3,26 @@ package com.example.gualbertoscolari.grupp3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.EditText;
 
 public class CreateQuestionActivity extends AppCompatActivity {
+
+    private int ID;
+
+    private String QUESTION;
+    private String OPTA;
+    private String OPTB;
+    private String OPTC;
+    private String OPTD;
+    private String CAT;
+    private String ANSWER;
+
+    private EditText inputQuestion;
+    private EditText inputOpta;
+    private EditText inputOptb;
+    private EditText inputOptc;
+    private EditText inputOptd;
+    private EditText inputcorrectAnswer;
 
 
     @Override
@@ -13,6 +30,16 @@ public class CreateQuestionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_question);
 
+        inputQuestion = (EditText) findViewById(R.id.question);
+        inputOpta = (EditText) findViewById(R.id.answer1);
+        inputOptb = (EditText) findViewById(R.id.answer2);
+        inputOptc = (EditText) findViewById(R.id.answer3);
+        inputOptd = (EditText) findViewById(R.id.answer4);
+        inputcorrectAnswer = (EditText) findViewById(R.id.correct_a);
     }
 
+    public void Onclixk(){
+        Question q = new Question(QUESTION,OPTA,OPTB, OPTC, OPTD, CAT, ANSWER);
+        DbHelper.addQuestion();
+    }
 }
