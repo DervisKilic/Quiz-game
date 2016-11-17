a99a575fc87f5420f79d19459b0efcf2016b274e
Dervis <dervis85@gmail.com>
2016-11-17 11:15:26 +0100

    added db and started on dbhelper, question

--- app/src/main/java/com/example/gualbertoscolari/grupp3/DbHelper.java
+++ app/src/main/java/com/example/gualbertoscolari/grupp3/DbHelper.java
@@ -1,4 +1,73 @@
 package com.example.gualbertoscolari.grupp3;
 
-public class DbHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    // Database Name
+    private static final String DATABASE_NAME = "questions_db";
+    // tasks table name
+    private static final String TABLE_QUEST = "quest";
+    // tasks Table Columns names
+    private static final String KEY_ID = "id";
+    private static final String KEY_QUES = "question";
+    private static final String KEY_ANSWER = "answer"; //correct option
+    private static final String KEY_OPTA= "opta"; //option a
+    private static final String KEY_OPTB= "optb"; //option b
+    private static final String KEY_OPTC= "optc"; //option c
+    private static final String KEY_OPTD= "optd"; //option d
+
+    private SQLiteDatabase dbase;
+
+    public DbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        dbase = sqLiteDatabase;
+
+        String sqlQuestions = "CREATE TABLE Questions (";
+        sqlQuestions += "_id INTEGER PRIMARY KEY AUTOINCREMENT,";
+        sqlQuestions += "Question VARCHAR(255) NOT NULL,";
+        sqlQuestions += "opta VARCHAR(255) NOT NULL,";
+        sqlQuestions += "optb VARCHAR(255) NOT NULL,";
+        sqlQuestions += "optc VARCHAR(255) NOT NULL,";
+        sqlQuestions += "optd VARCHAR(255) NOT NULL,";
+        sqlQuestions += "category VARCHAR(255) NOT NULL,";
+        sqlQuestions += "answer VARCHAR(255) NOT NULL";
+        sqlQuestions += ");";
+
+        sqLiteDatabase.execSQL(sqlQuestions);
+
+        addQuestion();
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void addQuestion(Question q) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues cvs = new ContentValues();
+        cvs.put("Question", q.getQUESTION());
+        cvs.put("opta", q.getOPTA());
+        cvs.put("optb", q.getOPTB());
+        cvs.put("optc", q.getOPTC());
+        cvs.put("optD", q.getOPTD());
+        cvs.put("category", q.getCATEGORY());
+        cvs.put("answer", q.getANSWER());
+
+        //this.addQuestion();
+       // Question q1 = new Question(q.getQUESTION(), q.getOPTA(), "Bajs", "Kiss", "Katt", "Sport", "Kilic");
+
+    }
+
 }
