7ecffd3c3b3240ebc35a1d8fabe9cbc21372d380
guasc0 <gual.scolari@hotmail.com>
2016-11-18 13:38:55 +0100

    Added new hint whean Category is created and a toast

--- app/src/main/java/com/example/gualbertoscolari/grupp3/CreateCategoryActivity.java
+++ app/src/main/java/com/example/gualbertoscolari/grupp3/CreateCategoryActivity.java
@@ -1,6 +1,7 @@
 package com.example.gualbertoscolari.grupp3;
 
 import android.content.Intent;
+import android.icu.util.ULocale;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
@@ -8,6 +9,7 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class CreateCategoryActivity extends AppCompatActivity {
@@ -21,6 +23,7 @@ public class CreateCategoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_category);
 
+
     }
 
     public void goToGameSettings(View v){
@@ -33,8 +36,11 @@ public class CreateCategoryActivity extends AppCompatActivity {
         String cat = category.getText().toString();
 
         DbHelper db = new DbHelper(getApplicationContext());
-
         db.addCategorys(cat);
+
+        category.setText("");
+        Toast.makeText(this, "You added a new category", Toast.LENGTH_SHORT).show();
+        category.setHint(this.getString(R.string.enter_category_name));
 
     }
 
