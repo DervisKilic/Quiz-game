7f956b70a4d6e0dcc7c53f994964ca68e88aaa32
Siniom <simon.riemertzon@gmail.com>
2016-11-18 12:02:44 +0100

    added toolbar to AboutActivity

--- app/src/main/java/com/example/gualbertoscolari/grupp3/AboutActivity.java
+++ app/src/main/java/com/example/gualbertoscolari/grupp3/AboutActivity.java
@@ -1,7 +1,10 @@
 package com.example.gualbertoscolari.grupp3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,4 +13,30 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.general_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.start_about_activity_ic:
+                Intent intent = new Intent(this, AboutActivity.class);
+                startActivity(intent);
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
--- app/src/main/java/com/example/gualbertoscolari/grupp3/MainGameActivity.java
+++ app/src/main/java/com/example/gualbertoscolari/grupp3/MainGameActivity.java
@@ -65,31 +65,13 @@ public class MainGameActivity extends AppCompatActivity {
         optCBtn.setText(currentQ.getOPTC());
         optDBtn.setText(currentQ.getOPTD());
 
-    public void displayQuestion(GameLogic) {
-        //Hämtar fråga från GameLogic och skriver ut den i TextView:n
-        //och skriver ut svaren på knapparna.
-
-        questiontv = (TextView) findViewById(R.id.question_tv);
-        // questiontv.settext() = information ifrån gamelogic/dbhelper
-
-        optABtn = (Button) findViewById(R.id.answer_btn_a);
-        optABtn.setText(/*information ifrån GameLogic/DBhelper*/);
-
-        optBBtn = (Button) findViewById(R.id.answer_btn_b);
-        optBBtn.setText(/*information ifrån GameLogic/DBhelper*/);
-
-        optCBtn = (Button) findViewById(R.id.answer_btn_c);
-        optCBtn.setText(/*information ifrån GameLogic/DBhelper*/);
-
-        optDBtn = (Button) findViewById(R.id.answer_btn_d);
-        optDBtn.setText(/*information ifrån GameLogic/DBhelper*/);
-    }
-
     public void checkCorrectAnswer(String optString) {
         // Ska användas OnClick på alla knappar när användaren gissar.
         // Ska kolla om den intrykta knappens text är lika med frågans correctAnswer.
 
-        if (optString.equals(questiontv.getText().toString()) ) {
+        numberOfAnsweredQ++;
+
+        if (optString.equals(questiontv.getText().toString())) {
             //Ifall man svarar rätt händer detta
 
             questiontv.setText("Hurra du svarade rätt på den här frågan");
@@ -136,21 +118,18 @@ public class MainGameActivity extends AppCompatActivity {
 
     public void btn_a_pressed(View view) {
         checkCorrectAnswer(optABtn.getText().toString());
-        numberOfAnsweredQ++;
     }
 
     public void btn_b_pressed(View view) {
+
         checkCorrectAnswer(optBBtn.getText().toString());
-        numberOfAnsweredQ++;
     }
 
     public void btn_c_pressed(View view) {
         checkCorrectAnswer(optCBtn.getText().toString());
-        numberOfAnsweredQ++;
     }
 
     public void btn_d_pressed(View view) {
         checkCorrectAnswer(optDBtn.getText().toString());
-        numberOfAnsweredQ++;
     }
 }
