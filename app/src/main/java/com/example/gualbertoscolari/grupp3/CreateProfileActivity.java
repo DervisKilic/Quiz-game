5c9b6d965df52aa6ee9a7ab6cd6d4fd4f4ce76ab
guasc0 <gual.scolari@hotmail.com>
2016-11-21 14:12:32 +0100

    Added new hint line when you add a new profile

--- app/src/main/java/com/example/gualbertoscolari/grupp3/CreateProfileActivity.java
+++ app/src/main/java/com/example/gualbertoscolari/grupp3/CreateProfileActivity.java
@@ -5,10 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreateProfileActivity extends AppCompatActivity {
 
     private EditText inputName;
+    private EditText profileName;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +22,26 @@ public class CreateProfileActivity extends AppCompatActivity {
 
     public void saveProfile(View view) {
 
-        inputName = (EditText) findViewById(R.id.profileName);
+        inputName = (EditText) findViewById(R.id.profile_name);
         String name = inputName.getText().toString();
 
         Profiles p = new Profiles(name, 0);
         DbHelper db = new DbHelper(getApplicationContext());
 
+        inputName.setText("");
+        Toast.makeText(this, "You added a new profile", Toast.LENGTH_SHORT).show();
+        inputName.setHint(this.getString(R.string.enter_category_name));
+
         db.addProfile(p);
     }
 
     public void goToGameSettings(View view) {
+        profileName = (EditText) findViewById(R.id.profile_name);
+        name = profileName.getText().toString();
+
         Intent playAgainIntent = new Intent(this, GameSettingsActivity.class);
         startActivity(playAgainIntent);
+
+
     }
 }
