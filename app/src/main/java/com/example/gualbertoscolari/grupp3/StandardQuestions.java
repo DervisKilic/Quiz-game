ad731615b6d0d2b3dc752a5fe2eb000627f75f5e
fredrikborjesson <fredrik.m.borjesson@gmail.com>
2016-11-21 14:27:27 +0100

    Created StandardQuestions.java with questions for testing app

---
+++ app/src/main/java/com/example/gualbertoscolari/grupp3/StandardQuestions.java
@@ -0,0 +1,17 @@
+package com.example.gualbertoscolari.grupp3;
+
+
+
+public class StandardQuestions {
+
+    Question q1 = new Question("Vilket trädslag är det i glasspinnen?", "Bok", "Alm", "Björk", "Al", "Natur", "Bok");
+    Question q2 = new Question("Ungefär hur många slag slår ett människohjärta per minut?","30-40","110-130","60-70","80-90","Natur","60-70");
+    Question q3 = new Question("När kom potatisen till Europa?", "På 1300--talet?", "På 1500-talet", "På 1700-talet", "På 1900-talet", "Natur", "På 1500-talet");
+    Question q4 = new Question("Vilken enhet mäter elektrisk spänning?","Volt","Ampere","Watt","Ohm","Natur","Volt");
+    Question q5 = new Question("Hur många mjölktänder har en tvååring?","16","30","20","198","Natur","20");
+    Question q6 = new Question("Påskdagen första söndagen efter första fullmånen efter..","Fastlagssöndagen","Vårdagjämningen","Askonsdagen","Nyår","Natur","Vårdagjämningen");
+    Question q7 = new Question("I vilken sport förekommer begreppen trap och skeet?","Lerduveskytte","Rodel","Cricket","Golf","Natur","Lerduveskytte");
+    Question q8 = new Question("Marilyn Monroes sista film hette vad?", "I hetaste laget", "Gasljus", "De missanpassade", "Star Wars", "Natur", "De missanpassade");
+    Question q9 = new Question("Var bor besättningen på ett fartyg?", "I skansen", "I durken", "I pentryt", "I fören", "Natur", "I skansen");
+    Question q10 = new Question("Var hölls den första världsutställningen 1851?", "I London", "I Amsterdam", "I New York", " I Paris", "Natur", "I London");
+}
