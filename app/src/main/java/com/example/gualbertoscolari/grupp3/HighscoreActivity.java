7c74005e4e309f799d5b16e7252e64c7f81bf22a
Siniom <simon.riemertzon@gmail.com>
2016-11-29 10:48:49 +0100

    Began working on a functional highScore. Next , connect it to the database.

--- app/src/main/java/com/example/gualbertoscolari/grupp3/HighscoreActivity.java
+++ app/src/main/java/com/example/gualbertoscolari/grupp3/HighscoreActivity.java
@@ -2,15 +2,34 @@ package com.example.gualbertoscolari.grupp3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 //Ska låta användaren lista highscoren efter specifik kategori eller alla.
 //Spinner funktion skall finnas för att välja mellan kategorierna.
 //
 public class HighscoreActivity extends AppCompatActivity {
+    ListView hsListV;
+    String profileName = "Simon";
+    int highscore = 10;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_highscore);
+         hsListV = (ListView) findViewById(R.id.hs_list);
+        ArrayList<String> highscores = new ArrayList<>();
+
+        highscores.add(0, ""+ profileName + highscore );
+
+        ArrayAdapter<String> listAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, highscores);
+
+            hsListV.setAdapter(listAdapter);
+
     }
 
     public void displayCategoriesInSpinner(){
