9de55c3325bf2ae42130e0ba7b727c270dd794e8
Dervis <dervis85@gmail.com>
2016-12-08 16:25:05 +0100

    added a animation to splash

--- app/src/main/java/com/example/gualbertoscolari/grupp3/SplashActivity.java
+++ app/src/main/java/com/example/gualbertoscolari/grupp3/SplashActivity.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 
 /**
@@ -12,6 +15,8 @@ import android.widget.ImageView;
  */
 public class SplashActivity extends Activity implements Animation.AnimationListener{
     private DbHelper db;
+    final float growTo = 1.2f;
+    final long duration = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +27,41 @@ public class SplashActivity extends Activity implements Animation.AnimationListe
         db = new DbHelper(this);
         db.addStandardItemsSQL();
         db.close();
+
+        ImageView test = (ImageView) findViewById(R.id.splash_logo);
+        ScaleAnimation grow = new ScaleAnimation(1, growTo, 1, growTo,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        grow.setDuration(duration / 2);
+        ScaleAnimation shrink = new ScaleAnimation(growTo, 1, growTo, 1,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        shrink.setDuration(duration / 2);
+        shrink.setStartOffset(duration / 2);
+        AnimationSet growAndShrink = new AnimationSet(true);
+        growAndShrink.setInterpolator(new LinearInterpolator());
+        growAndShrink.addAnimation(grow);
+        growAndShrink.addAnimation(shrink);
+        test.startAnimation(growAndShrink);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         final Thread timer= new Thread(){
             public void run(){
                 try{
 
-                    ImageView rotate_image =(ImageView) findViewById(R.id.imageView);
+                    ImageView rotate_image =(ImageView) findViewById(R.id.rotateSplashImg);
                     RotateAnimation rotate = new RotateAnimation(30, 360, Animation.RELATIVE_TO_SELF, 0.5f,  Animation.RELATIVE_TO_SELF, 0.5f);
                     rotate.setDuration(2500);
                     rotate_image.startAnimation(rotate);
